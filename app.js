78fed58cbb1becb918ee7c373496d2e283c94058
Uellington Santos <uellington.santos@outlook.com>
2025-02-04 21:08:59 -0300

    Função que adiciona nomes ao array

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+//array para armaznar os nomes dos amigos secretos
+let amigosSecrectos = [];
+
+//função para adicionar amigos ao array de amigos secretos
+function adicionarAmigoSecreto(){
+    //Adiciona o nome fornecido no input ao array de amigos secretos
+
+    let nomeFornecido = document.querySelector('.input-name'); 
+
+    if (nomeFornecido.value != "") {
+        amigosSecrectos.push(nomeFornecido.value);
+        //retoma a mensagem original, pedindo ao usuário para digitar o nome dos amigos
+        let mensagemNomeValido = document.getElementById('mensagemNome');
+        mensagemNomeValido.innerHTML = "Digite o próximo nome.";
+        //limpa o input após adicionar o nome ao array
+        nomeFornecido.value = '' ;
+        
+    }
+    else {
+        //caso o usuário não digite um nome válido, uma mensagem de erro é exibida
+        let mensagemNomeValido = document.getElementById('mensagemNome');
+        mensagemNomeValido.innerHTML = "Por favor, digite um nome válido!";
+    }
+
+}
